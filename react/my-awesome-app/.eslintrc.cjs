977d405191b8710822488289fece17659449630e
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2023-07-22 20:23:02 +0200

    add linting

--- react/my-awesome-app/.eslintrc.cjs
+++ react/my-awesome-app/.eslintrc.cjs
@@ -1,8 +1,15 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    'cypress/globals': true,
+    node: true
+  },
   extends: [
     'eslint:recommended',
+    "plugin:cypress/recommended",
+    "plugin:mocha/recommended",
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
@@ -10,11 +17,16 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [
+    'cypress',
+    'mocha',
+    'react-refresh'
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'mocha/no-mocha-arrows': 'off'
   },
 }
